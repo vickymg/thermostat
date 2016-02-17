91a43d1fe76a027baf5a744b6571ec065c8c862b
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-17 11:11:04 +0000

    Passing spec tests for increase and decrease temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,6 +11,15 @@ describe('Thermostat', function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
-  
+  it('can increase the temperature', function() {
+    thermostat.increaseTemperature();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('can decrease the temperature', function() {
+    thermostat.decreaseTemperature();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
 };
+
+Thermostat.prototype.increaseTemperature = function() {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.decreaseTemperature = function () {
+  this.temperature -= 1;
+};
