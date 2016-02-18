f27ffe30c6b8a4110851399c74c5b5ac053c38cc
Ina Tsetsova <inatsetsova@gmail.com>
2016-02-18 12:40:57 +0000

    prett and jquery sorta works

--- jasmine/src/Thermostat.js
+++ jasmine/src/Thermostat.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var maxTempHash = {savingOff:32, savingOn:25}
-var tempRange= {high: 32, medium: 25, low: 18}
-
 function Thermostat() {
+  this.maxTempHash = {savingOff:32, savingOn:25}
+  this.tempRange= {high: 32, medium: 25, low: 18}
   this.DEFAULT_TEMP = 20;
   this.temperature = this.DEFAULT_TEMP
   this._minTemp = 10;
-  this._maxTemp = maxTempHash.savingOn;
+  this._maxTemp = this.maxTempHash.savingOn;
   this.savingMode = true;
   this.screenColor = this.determineColor();
 };
+
 
 Thermostat.prototype.resetTemp = function(){
   this.temperature = this.DEFAULT_TEMP;
@@ -40,25 +40,29 @@ Thermostat.prototype.savingModeOff = function() {
 }
 
 Thermostat.prototype.savingModeOn = function() {
+  if (this.temperature  > this.maxTempHash.savingOn){
+    this.temperature  = this.maxTempHash.savingOn
+  }
+
   this.savingMode = true;
   this.determineMaxTemp();
 }
 
 Thermostat.prototype.determineMaxTemp = function(){
   if(this.savingMode === true) {
-    return this._maxTemp = maxTempHash.savingOn;
+    return this._maxTemp = this.maxTempHash.savingOn;
     // return  temperatureHash["savings"];
   } else {
-    return this._maxTemp = maxTempHash.savingOff;
+    return this._maxTemp = this.maxTempHash.savingOff;
     // return temperatureHash["noSavings"];
    }
 }
 
 Thermostat.prototype.determineColor = function(){
-  if(this.temperature < tempRange.low) {
+  if(this.temperature < this.tempRange.low) {
     return "green";
   }
-  else if(this.temperature < tempRange.medium) {
+  else if(this.temperature < this.tempRange.medium) {
     return "yellow";
   }
   else {
