bf2cbc454bdb39413603cdd8989d596b20923ac7
Ina Tsetsova <inatsetsova@gmail.com>
2016-02-19 12:28:59 +0000

    prettyyy

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -3,10 +3,22 @@ $( document ).ready(function() {
     	function displayTemp() {
     		$('#temperature').text(thermostat.temperature);
     		$('#notification').css("background-color", thermostat.determineColor());
-    	}
+      }
 
     	var thermostat = new Thermostat();
     	displayTemp();
+
+      $.get("http://api.openweathermap.org/data/2.5/weather?q=mumbai&APPID=b7c5477d0a1d7e7fc8feed92153db903&units=metric", function(result){
+        var temp = Math.round(result.main.temp)
+        $('#api-temp').text(temp);
+        if (temp >= 25){
+          $('#outdoor-temp').toggleClass("warm-temp");
+        } else if (temp > 10) {
+          $('#outdoor-temp').toggleClass("regular-temp");
+        } else {
+          $('#outdoor-temp').toggleClass("cold-temp");
+        }
+      });
 
       $( "#up-arrow" ).click(function( event ) {
         thermostat.increaseTemp();
